90dd8fa79195c22e6eac2c3b91ebe3b5527af24b
daguowang <1104182880@qq.com>
2016-01-15 14:57:24 +0800

    worte a quadratic file

---
+++ quadratic.py
@@ -0,0 +1,12 @@
+import math
+
+def quadratic(a,b,c):
+    if(b*b-4*a*c)>0:
+        x1=(-b+math.sqrt(b*b-4*a*c))/2*a
+        x2=(-b-math.sqrt(b*b-4*a*c))/2*a
+        return(x1,x2)
+    elif(b*b-4*a*c)==0:
+        x=(-b)/2*a
+        return x
+    else:
+        return('此方程没有根')
